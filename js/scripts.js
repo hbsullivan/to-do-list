56ea6c1e9729448ba1a19b349265be3bc82fea0a
Henry Sullivan <sullivanbhenry@gmail.com>
2022-10-31 16:29:53 -0700

    added ui logic and content to index.html to display todo list to user
    Co-authored-by: Kirsten Opstad <kirsten.opstad@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+// Initialize global "todo"
+let todo = new ToDo();
+
+// Business Logic
+function createNewTask(task) {
+  let newTask = new Task(task, false) ;
+  return newTask;
+};
+
 function Task(chore, completed) {
   this.chore = chore;
   this.completed = completed;
@@ -29,3 +38,31 @@ ToDo.prototype.removeTask = function(id) {
   delete this.tasks[id];
   return true;
 };
+
+// User Interface Logic
+
+function handleTaskAdd() {
+  // Retrieve input
+  const task = document.getElementById("toDoInput").value;
+  // Create task
+  todo.addTask(createNewTask(task));
+  // Display task
+  const todoArea = document.getElementById("todo-list");
+  // const ul = document.createElement("ul");
+  const li = document.createElement("li");
+  const index = todo.currentId;
+  // Add task name to li
+  li.append(todo.tasks[index].chore);
+  // Add item to list
+  todoArea.append(li);
+  // add list to todo div
+  // todoArea.append(ul);
+}
+
+window.addEventListener("load", function() {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function() {
+    event.preventDefault();
+    handleTaskAdd();
+  });
+})
